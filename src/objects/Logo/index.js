4b713cd9d52cffc7ac00d59e1719f6bc83d46fb0
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-06-07 11:18:26 -0300

    Cria Logo e adiciona no App.js

---
+++ src/objects/Logo/index.js
@@ -0,0 +1,6 @@
+import React, {Fragment} from 'react';
+import LogoImage from '../../img/logo-lucas.png';
+
+const Logo = () => (<img src={LogoImage} alt='Logo do Lucas Efrens' />);
+
+export default Logo;

bdbaf1852a6e35b093fcaf331b2b8e7cee998d53
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-06-07 09:32:21 -0300

    Faz o link sobre

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite o <code>src/App.js</code> e salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
-      </header>
-    </div>
+    <a href='#to-do' className='action'>Sobre</a>
   );
 }
 

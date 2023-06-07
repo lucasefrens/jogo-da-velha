1e0c3862930e99950daa9a095bebd29696878de4
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-06-07 12:50:09 -0300

    Cria o component Header sem o CSS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React from "react"; 
-import Logo from "./objects/Logo";
-import Sobre from './objects/Sobre';
-import Menu from "./objects/Menu";
+import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <Logo />
-      <Sobre />
-      <Menu />
-    </>
+    <Header />
   );
 }
 

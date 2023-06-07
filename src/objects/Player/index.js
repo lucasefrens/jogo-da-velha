b793d0df22fd69c968f6cf7ec1fcd1657799898e
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-06-07 15:12:02 -0300

    Adicionar estilo ao object Player

--- src/objects/Player/index.js
+++ src/objects/Player/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import './styles.css'
+import playerX from '../../img/player-x.png'; 
 
 const Player = () => (
-    <button className="player">X</button>
+    <button className="player">
+        <img src={playerX} alt="Jogador X"/>
+    </button>
 );
 
 export default Player;

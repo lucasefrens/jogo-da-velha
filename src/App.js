0d95c4bbd0b233921bf914bf1f6d6073b4aed5bd
Lucas Martins Efrens <lucas_efrens@outlook.com>
2023-06-09 13:07:26 -0300

    Cria o object About

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css'
 
 import Checkbox from "./objects/Checkbox";
+import About from "./objects/About";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 
@@ -10,6 +11,8 @@ const App = () => (
     <Header />
     <Hashtag />
     <Checkbox id='show' value='show' content='Mostrar eventos'/>
+
+    <About />
   </main>
 );
 
---
+++ src/objects/About/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const About = () => (
+    <article className=''>Lucas Efrens</article>
+);
+
+export default About;
